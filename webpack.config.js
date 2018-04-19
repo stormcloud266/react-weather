039f8adc599a50beb802b3900d37afa36b5af62a
stormcloud266 <allprt89@gmail.com>
2018-04-19 12:55:12 -0600

    built for gitpages

--- webpack.config.js
+++ webpack.config.js
@@ -20,37 +20,41 @@ module.exports = (env) => {
 
         // figure out how to get this working
 
-        // {
-        //   test: /\.s?css$/,
-        //   use: CSSExtract.extract({
-        //     use: [
-        //       'css-loader',
-        //       'sass-loader'
-        //     ]
-        //   })
-        // },
-        {
-            test: /\.(png|eot|svg|ttf|woff|woff2)$/,
-            exclude: /node_modules/,
-            loader: 'file-loader'
-        },
-
         {
           test: /\.s?css$/,
-          use: [
-            'style-loader',
-            'css-loader',
-            'sass-loader'
-          ]
+          use: CSSExtract.extract({
+            use: [
+              'css-loader',
+              'sass-loader'
+            ]
+          })
+        },
+        {
+          test: /\.(eot|svg|ttf|woff|woff2)$/,
+          exclude: /node_modules/,
+          loader: 'file-loader'
+        },
+        {
+          test: /\.(?:ico|gif|png|jpg|jpeg|webp)$/,
+          use: 'url-loader',
         }
+
+        // {
+        //   test: /\.s?css$/,
+        //   use: [
+        //     'style-loader',
+        //     'css-loader',
+        //     'sass-loader'
+        //   ]
+        // }
       ]
     },
     resolve: {
       extensions: ['*', '.js', '.jsx']
     },
     output: {
-      path: __dirname + '/dist',
-      publicPath: '/',
+      path: __dirname + '/docs',
+      publicPath: './',
       filename: 'bundle.js'
     },
     plugins: [
